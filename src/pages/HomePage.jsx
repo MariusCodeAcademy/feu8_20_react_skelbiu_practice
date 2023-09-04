34ecb7afcb94b3516d92610d3bef9753da05b16b
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-09-04 10:38:37 +0300

    emty pages

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div className='container'>
+      <h1 className='text-3xl mb-8'>HomePage</h1>
+      <p>welcome to out adds</p>
+    </div>
+  );
+}
---
+++ src/pages/MyAccountPage.jsx
@@ -0,0 +1,8 @@
+export default function MyAccountPage() {
+  return (
+    <div className='container'>
+      <h1 className='text-3xl mb-8'>MyAccountPage</h1>
+      <p>your adds here</p>
+    </div>
+  );
+}
