e3951387469bb07ba86b34eb9c2467ef8c78b7ef
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-09-04 11:27:37 +0300

    firebase app + env

--- src/pages/CreateAdd.jsx
+++ src/pages/CreateAdd.jsx
@@ -1,5 +1,7 @@
+import { app } from '../firebase/firebase';
+
 export default function CreateAdd() {
-  console.log('firebse app');
+  console.log('firebse app', app);
   return (
     <div className='container'>
       <h1 className='text-3xl mb-4 pt-4'>CreateAdd</h1>
