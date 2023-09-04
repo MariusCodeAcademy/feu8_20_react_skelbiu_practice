d3cb630463b5118bd326a87831ffc242ebcdc19f
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-09-04 10:47:25 +0300

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import MyAccountPage from './pages/MyAccountPage';
 
 export default function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/my-adds' element={<MyAccountPage />} />
+      </Routes>
     </div>
   );
 }
