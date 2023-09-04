e4fb0f33e1e689a5df7669e1cbb04f68ae1cf26d
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-09-04 10:42:14 +0300

    container

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div className='container'>
-      <h1 className='text-3xl mb-8'>HomePage</h1>
+      <h1 className='text-3xl mb-4 pt-4'>HomePage</h1>
       <p>welcome to out adds</p>
     </div>
   );
--- src/pages/MyAccountPage.jsx
+++ src/pages/MyAccountPage.jsx
@@ -1,7 +1,7 @@
 export default function MyAccountPage() {
   return (
     <div className='container'>
-      <h1 className='text-3xl mb-8'>MyAccountPage</h1>
+      <h1 className='text-3xl mb-4 pt-4'>MyAccountPage</h1>
       <p>your adds here</p>
     </div>
   );
