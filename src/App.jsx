c2f53b45d1a73cc6cc0c0aec5ed8455b6ff40eeb
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-09-04 10:55:52 +0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import MyAccountPage from './pages/MyAccountPage';
+import Header from './components/layout/Header';
 
 export default function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/my-adds' element={<MyAccountPage />} />
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header className='container flex justify-between items-center '>
+      <Link className='text-2xl' to={'/'}>
+        Our<strong>Adds</strong>
+      </Link>
+      <nav>
+        <NavLink className={'text-lg px-3 py-2 hover:bg-slate-200'} to={'/'}>
+          Adds
+        </NavLink>
+        <NavLink
+          className={'text-lg px-3 py-2 hover:bg-slate-200'}
+          to={'/my-adds'}
+        >
+          My adds
+        </NavLink>
+        <NavLink
+          className={'text-lg px-3 py-2 hover:bg-slate-200'}
+          to={'/login'}
+        >
+          Login
+        </NavLink>
+      </nav>
+    </header>
+  );
+}
