e83796db6ff9d420324c33f3ebd2cdd991285c39
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-09-05 10:29:11 +0300

    formik

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,9 +1,22 @@
+import { useFormik } from 'formik';
+
 export default function LoginForm() {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: (values) => {
+      console.log('supildytos reiksmes ===', values);
+    },
+  });
   return (
     <div>
-      <form className='max-w-xs'>
+      <form onSubmit={formik.handleSubmit} className='max-w-xs'>
         <div className='mb-2'>
           <input
+            onChange={formik.handleChange}
+            value={formik.values.email}
             className='border border-slate-500 px-4 py-2 w-full rounded-md'
             type='text'
             id='email'
@@ -12,6 +25,8 @@ export default function LoginForm() {
         </div>
         <div className='mb-2'>
           <input
+            onChange={formik.handleChange}
+            value={formik.values.password}
             className='border border-slate-500 px-4 py-2 w-full rounded-md'
             type='password'
             id='password'
