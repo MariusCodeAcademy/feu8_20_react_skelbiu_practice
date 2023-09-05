781fdb3279057c965a5d9805136c57836f96cdca
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-09-05 09:56:10 +0300

    protecting routes and links

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import MyAccountPage from './pages/MyAccountPage';
 import Header from './components/layout/Header';
 import CreateAdd from './pages/CreateAdd';
+import LoginPage from './pages/LoginPage';
+import { useAuth } from './store/AuthProvider';
 
 export default function App() {
+  const ctx = useAuth();
   return (
     <div>
       <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/create-add' element={<CreateAdd />} />
-        <Route path='/my-adds' element={<MyAccountPage />} />
+        <Route
+          path='/create-add'
+          element={
+            ctx.isUserLoggedIn ? <CreateAdd /> : <Navigate to={'/login'} />
+          }
+        />
+        <Route
+          path='/my-adds'
+          element={
+            ctx.isUserLoggedIn ? <MyAccountPage /> : <Navigate to={'/login'} />
+          }
+        />
+        <Route path='/login' element={<LoginPage />} />
+        <Route
+          path='*'
+          element={
+            <div>
+              <h1>404</h1>
+              <p>page not found</p>
+            </div>
+          }
+        />
       </Routes>
     </div>
   );
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  return (
+    <div className='container'>
+      <h1 className='text-3xl mb-4 pt-4'>Login here</h1>
+      <p>welcome to out adds</p>
+    </div>
+  );
+}
