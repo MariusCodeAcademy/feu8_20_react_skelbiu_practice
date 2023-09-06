5d53d996f30ed8947018c98078c71bfdc46f8cea
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-09-06 11:29:13 +0300

    li rm

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -48,39 +48,8 @@ export default function HomePage() {
       <ul className='grid grid-cols-3 gap-3'>
         {addsArr.map((addObj) => (
           <SingleAddCard key={addObj.id} item={addObj} />
-          // <li
-
-          //   className={`border-[1px] px-4 py-3 inline-block ${isMine(
-          //     userUid,
-          //     ctx.userUid
-          //   )}`}
-          // >
-          //   <h2 className='font-bold text-2xl'>{title}</h2>
-          //   <p>{price}eur</p>
-          //   <p>userUid: {userUid}</p>
-          //   <Link
-          //     className='border border-slate-200 px-2 py-1 mt-3 inline-block bg-slate-600 text-white'
-          //     to={`/adds/${id}`}
-          //   >
-          //     Read more
-          //   </Link>
-          //   {userUid === ctx.userUid && (
-          //     <button
-          //       onClick={() => deleteFire(id)}
-          //       className='border border-slate-200 px-2 py-1 mt-3 inline-block bg-red-600 text-white'
-          //     >
-          //       delete
-          //     </button>
-          //   )}
-          // </li>
         ))}
       </ul>
     </div>
   );
-}
-
-function isMine(elId, userId) {
-  if (elId === userId) {
-    return 'shadow-md bg-amber-100';
-  }
 }
