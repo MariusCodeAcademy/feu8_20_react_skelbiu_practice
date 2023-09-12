7c0e7f437f29cbcbb1695a51f3e4e959c1ce4490
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-09-12 15:24:10 +0300

    burger

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,7 +2,8 @@ import { Link, NavLink } from 'react-router-dom';
 import { useAuth } from '../../store/AuthProvider';
 import { getAuth, signOut } from 'firebase/auth';
 import toast from 'react-hot-toast';
-
+import { FaHamburger } from 'react-icons/fa';
+import { useState } from 'react';
 function OneLink(props) {
   return (
     <NavLink
@@ -30,6 +31,7 @@ function logoutFire() {
 }
 
 export default function Header() {
+  const [isMobileOpen, setIsMobileOpen] = useState(false);
   // atvaizduoti prisijungusio userio email is conteksto
   const ctx = useAuth();
   console.log('ctx ===', ctx);
@@ -38,7 +40,13 @@ export default function Header() {
       <Link className='text-2xl' to={'/'}>
         Our<strong>Adds</strong>
       </Link>
-      <nav>
+      <nav
+        className={` lg:block ${
+          isMobileOpen
+            ? 'flex flex-col absolute top-9 bg-slate-200 right-0'
+            : 'hidden'
+        }`}
+      >
         <OneLink to={'/'} title={'Adds'} />
         {ctx.isUserLoggedIn && (
           <>
@@ -56,6 +64,9 @@ export default function Header() {
           <p className='inline-block text-lg px-3 py-2'>{ctx.email}</p>
         )}
       </nav>
+      <div className='block lg:hidden '>
+        <FaHamburger onClick={() => setIsMobileOpen(!isMobileOpen)} />
+      </div>
     </header>
   );
 }
